2ebb6cbfb6b0e2655a5254138ce4905564a78e04
sergei kuranov <sergei_kuranov@epam.com>
2016-12-17 14:34:26 +0400

    first commit

---
+++ src/lesson1/Lesson1.java
@@ -0,0 +1,127 @@
+package lesson1;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Zivert on 12/4/2016.
+ */
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+    /**
+     * @author Speakjava (Simon Ritter)
+     */
+    public class Lesson1 {
+        /**
+         * Run the exercises to ensure we got the right answers
+         */
+        public void runExercises() {
+            System.out.println("JDK 8 Lambdas and Streams MOOC Lesson 1");
+            System.out.println("Running exercise 1 solution...");
+            exercise1();
+            System.out.println("Running exercise 2 solution...");
+            exercise2();
+            System.out.println("Running exercise 3 solution...");
+            exercise3();
+            System.out.println("Running exercise 4 solution...");
+            exercise4();
+            System.out.println("Running exercise 5 solution...");
+            exercise5();
+        }
+
+        /**
+         * All exercises should be completed using Lambda expressions and the new
+         * methods added to JDK 8 where appropriate. There is no need to use an
+         * explicit loop in any of the code. Use method references rather than full
+         * lambda expressions wherever possible.
+         */
+        /**
+         * Exercise 1
+         *
+         * Create a string that consists of the first letter of each word in the list
+         * of Strings provided.
+         */
+        private void exercise1() {
+            List<String> list = Arrays.asList(
+                    "alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+            StringBuilder sb = new StringBuilder();
+            list.forEach(str -> sb.append(str.charAt(0)));
+            System.out.printf(sb.toString()+"\n");
+
+    /* YOUR CODE HERE */
+        }
+
+        /**
+         * Exercise 2
+         *
+         * Remove the words that have odd lengths from the list.
+         */
+        private void exercise2() {
+            List<String> list = new ArrayList<>(Arrays.asList(
+                    "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
+            list.removeIf(str-> str.length()%2!=0);
+            list.forEach(System.out::println);
+            /* YOUR CODE HERE */
+        }
+
+        /**
+         * Exercise 3
+         *
+         * Replace every word in the list with its upper case equivalent.
+         */
+        private void exercise3() {
+            List<String> list = new ArrayList<>(Arrays.asList(
+                    "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
+            list.replaceAll(String::toUpperCase);
+            list.forEach(System.out::println);
+    /* YOUR CODE HERE */
+        }
+
+        /**
+         * Exercise 4
+         *
+         * Convert every key-value pair of the map into a string and append them all
+         * into a single string, in iteration order.
+         */
+        private void exercise4() {
+            Map<String, Integer> map = new TreeMap<>();
+            map.put("c", 3);
+            map.put("b", 2);
+            map.put("a", 1);
+            StringBuilder sb = new StringBuilder();
+            map.forEach((str, num) -> sb.append(str));
+            System.out.println(sb.toString()+ "\n");
+    /* YOUR CODE HERE */
+        }
+
+        /**
+         * Exercise 5
+         *
+         * Create a new thread that prints the numbers from the list.
+         */
+        private void exercise5() {
+            List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            Runnable run = () -> list.forEach(System.out::println);
+            run.run();
+
+
+    /* YOUR CODE HERE */
+        }
+
+        /**
+         * Main entry point for application
+         *
+         * @param args the command line arguments
+         */
+        public static void main(String[] args) {
+            Lesson1 lesson = new Lesson1();
+            lesson.runExercises();
+        }
+    }
+
---
+++ src/lesson2/Lesson2.java
@@ -0,0 +1,174 @@
+/**
+ * Copyright © 2016, Oracle and/or its affiliates. All rights reserved.
+ * <p>
+ * JDK 8 MOOC Lesson 2 homework
+ */
+package lesson2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Speakjava (Simon Ritter)
+ */
+public class Lesson2 {
+    private static final String WORD_REGEXP = "[- .:,]+";
+
+    /**
+     * Run the exercises to ensure we got the right answers
+     *
+     * @throws java.io.IOException
+     */
+    public void runExercises() throws IOException {
+        System.out.println("JDK 8 Lambdas and Streams MOOC Lesson 2");
+        System.out.println("Running exercise 1 solution...");
+        exercise1();
+        System.out.println("Running exercise 2 solution...");
+        exercise2();
+        System.out.println("Running exercise 3 solution...");
+        exercise3();
+        System.out.println("Running exercise 4 solution...");
+        exercise4();
+        System.out.println("Running exercise 5 solution...");
+        exercise5();
+        System.out.println("Running exercise 6 solution...");
+        exercise6();
+        System.out.println("Running exercise 7 solution...");
+        exercise7();
+    }
+
+    /**
+     * Exercise 1
+     * <p>
+     * Create a new list with all the strings from original list converted to
+     * lower case and print them out.
+     */
+    private void exercise1() {
+        List<String> list = Arrays.asList(
+                "The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG");
+        List finalList = list.stream()
+                .map(String::toLowerCase)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+    /* YOUR CODE HERE */
+    }
+
+    /**
+     * Exercise 2
+     * <p>
+     * Modify exercise 1 so that the new list only contains strings that have an
+     * odd length
+     */
+    private void exercise2() {
+        List<String> list = Arrays.asList(
+                "The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG");
+        List finalList = list.stream()
+                .filter(str -> str.length() % 2 != 0)
+                .map(String::toLowerCase)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+    /* YOUR CODE HERE */
+    }
+
+    /**
+     * Exercise 3
+     * <p>
+     * Join the second, third and forth strings of the list into a single string,
+     * where each word is separated by a hyphen (-). Print the resulting string.
+     */
+    private void exercise3() {
+        List<String> list = Arrays.asList(
+                "The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
+        System.out.println(list.stream()
+                .skip(1)
+                .limit(4).reduce((mainStr, str) -> mainStr + "-" + str).get());
+    /* YOUR CODE HERE */
+    }
+
+    /**
+     * Count the number of lines in the file using the BufferedReader provided
+     */
+    private void exercise4() throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(
+                Paths.get("SonnetI.txt"), StandardCharsets.UTF_8)) {
+            System.out.println(reader.lines().count());
+      /* YOUR CODE HERE */
+        }
+    }
+
+    /**
+     * Using the BufferedReader to access the file, create a list of words with
+     * no duplicates contained in the file.  Print the words.
+     * <p>
+     * HINT: A regular expression, WORD_REGEXP, is already defined for your use.
+     */
+    private void exercise5() throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(
+                Paths.get("SonnetI.txt"), StandardCharsets.UTF_8)) {
+            List finslList = reader
+                    .lines()
+                    .flatMap(str -> Stream.of(str.split(WORD_REGEXP)))
+                    .distinct()
+                    .peek(System.out::println)
+                    .collect(Collectors.toList());
+      /* YOUR CODE HERE */
+        }
+    }
+
+    /**
+     * Using the BufferedReader to access the file create a list of words from
+     * the file, converted to lower-case and with duplicates removed, which is
+     * sorted by natural order.  Print the contents of the list.
+     */
+    private void exercise6() throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(
+                Paths.get("SonnetI.txt"), StandardCharsets.UTF_8)) {
+            List finslList = reader
+                    .lines()
+                    .flatMap(str -> Stream.of(str.split(WORD_REGEXP)))
+                    .distinct()
+                    .sorted()
+                    .peek(System.out::println)
+                    .collect(Collectors.toList());
+      /* YOUR CODE HERE */
+        }
+    }
+
+    /**
+     * Modify exercise6 so that the words are sorted by length
+     */
+    private void exercise7() throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(
+                Paths.get("SonnetI.txt"), StandardCharsets.UTF_8)) {
+            List finslList = reader
+                    .lines()
+                    .flatMap(str -> Stream.of(str.split(WORD_REGEXP)))
+                    .distinct()
+                    .sorted((x, y) -> x.length() - y.length())
+                    .peek(System.out::println)
+                    .collect(Collectors.toList());
+      /* YOUR CODE HERE */
+        }
+    }
+
+    /**
+     * Main entry point for application
+     *
+     * @param args the command line arguments
+     * @throws IOException If file access does not work
+     */
+    public static void main(String[] args) throws IOException {
+        Lesson2 lesson = new Lesson2();
+        lesson.runExercises();
+    }
+}
+
